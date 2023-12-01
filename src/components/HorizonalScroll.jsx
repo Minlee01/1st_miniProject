3cb9efabf10c046f4ef96ae167e6db73d4ee0a40
Minkyung Lee <minlee4321@gmail.com>
2023-12-01 14:25:37 +0900

    HorizontalScroll_Error_noreply

--- src/components/HorizonalScroll.jsx
+++ src/components/HorizonalScroll.jsx
@@ -3,3 +3,91 @@
 그 다음엔 https://wazacs.tistory.com/26
 https://velog.io/@071yoon/React-Horizontal-Scroll-%EA%B5%AC%ED%98%84   참고...
 */ 
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import React, {useState} from "react";
+import styled from "styled-components";
+import 'react-horizontal-scrolling-menu/dist/styles.css';
+
+
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: `element-${ind}` }));
+
+function HorizontalScroll() {
+  const [items, setItems] = React.useState(getItems);
+  const [selected, setSelected] = React.useState([]);
+  const [position, setPosition] = React.useState(0);
+
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick =
+    (id) =>
+    ({ getItemById, scrollToItem }) => {
+      const itemSelected = isItemSelected(id);
+
+      setSelected((currentSelected) =>
+        itemSelected
+          ? currentSelected.filter((el) => el !== id)
+          : currentSelected.concat(id)
+      );
+    };
+
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {items.map(({ id }) => (
+        <Card
+          itemId={id} // NOTE: itemId is required for track items
+          title={id}
+          key={id}
+          onClick={handleClick(id)}
+          selected={isItemSelected(id)}
+        />
+      ))}
+    </ScrollMenu>
+  );
+}
+
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } =
+    React.useContext(VisibilityContext);
+
+  return (
+    <LeftArrow onClick = {() => scrollPrev()}>←</LeftArrow>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+
+  return (
+    <RightArrow onClick = {() => scrollNext()}>→</RightArrow>
+  );
+}
+
+function Card({ onClick, selected, title, itemId }) {
+  const visibility = React.useContext(VisibilityContext);
+
+  return (
+    <div
+      onClick={() => onClick(visibility)}
+      style={{
+        width: '160px',
+      }}
+      tabIndex={0}
+    >
+      <div className="card">
+        <div>{title}</div>
+        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+        <div>selected: {JSON.stringify(!!selected)}</div>
+      </div>
+      <div
+        style={{
+          height: '200px',
+        }}
+      />
+    </div>
+  );
+}
+
+export default HorizontalScroll;
--- src/pages/shop/Page5.jsx
+++ src/pages/shop/Page5.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import testContainer from "../../components/testContainer"
 import { useNavigate } from "react-router-dom";
+import HorizontalScroll from "../../components/HorizonalScroll";
 
 function Page5(props){ 
     //const navigate = useNavigate();
 
     //const{} = props;
         return (
-            <testContainer />
+            <HorizontalScroll />
         
     )
     
